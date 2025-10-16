df92e7066de1605e395262b6b831e6aba8587cb7
POLK BRANDO VERNAZA QUIÑONEZ <83431658+o698polk@users.noreply.github.com>
2025-10-16 15:44:29 -0500

    Actualizacion de repositorio

---
+++ POLK VERNAZA/Proyectos/index.php
@@ -0,0 +1,5 @@
+<?php
+// index.php - Nombre: TuNombre
+echo "<!doctype html>";
+echo "<html lang='es'><head><meta charset='utf-8'><title>Index</title></head><body><h1>Hola desde index.php</h1></body></html>";
+?>
