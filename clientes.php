ab65a0eaa42a21ad9a6a4c6dfaad8970777d2a89
jhostynelian1009 <jhostynelianb@gmail.com>
2025-10-15 19:15:07 -0500

    actualizacion tabla clientes

---
+++ clientes.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>My PHP Page</title>
+        <link rel="stylesheet" type="text/css" href="styles.css">
+        <script src="script.js"></script>
+    </head>
+    <body>
+  <h1> clientes  </h1>
+        </body>
+</html>
