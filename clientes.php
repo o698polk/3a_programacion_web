a66c7848d9df2d9cc0bf03d3d53f3d81d172fa6a
POLK BRANDO VERNAZA QUIÑONEZ <83431658+o698polk@users.noreply.github.com>
2025-10-15 19:10:54 -0500

    Nuevos cambios, archivo clientes y productos

---
+++ clientes.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Cliente</title>
+        <link rel="stylesheet" type="text/css" href="styles.css">
+        <script src="script.js"></script>
+    </head>
+    <body>
+
+       
+ </body>
+</html>
---
+++ productos.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Productos</title>
+        <link rel="stylesheet" type="text/css" href="styles.css">
+        <script src="script.js"></script>
+    </head>
+    <body>
+
+       
+ </body>
+</html>
