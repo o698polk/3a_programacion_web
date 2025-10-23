a5c24708fae31938bcec80f17f18fa1736bd260b
POLK BRANDO VERNAZA QUIÑONEZ <83431658+o698polk@users.noreply.github.com>
2025-10-22 19:35:08 -0500

    Ultimos cambios en la clases de programacion web

---
+++ POLK VERNAZA/Proyectos/indexOld.php
@@ -0,0 +1,10 @@
+<?php
+
+// index.php - Nombre: TuNombre
+echo "<!doctype html>";
+echo "<html lang='es'><head><meta charset='utf-8'><title>Index</title></head><body><h1 style='color: red;'> <style>
+body {
+    background-color: #f0f0f0;
+}
+</style>   Hola desde index.php</h1></body></html>";
+?>
