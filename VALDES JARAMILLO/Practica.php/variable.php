b3144358fcd4b2046dbe5fe2f96973925063b12f
Yokabeth Valdes <djb502660@gmail.com>
2025-10-23 21:39:06 -0500

    ..

---
+++ VALDES JARAMILLO/Practica.php/variable.php
@@ -0,0 +1,25 @@
+<?php
+$nombre="";
+if(isset($_POST['nombre'])){
+$nombre=$_POST['nombre'];
+}
+echo $nombre;
+?>
+<html>
+   <head>
+      <title>Variables</title>
+      <style>
+         h1 {
+            color: blue; 
+         }
+      </style>
+   </head>
+<body>
+    <h1>FORMULARIO</h1>
+     <form action="varibles.php" method="post">
+          <label for="nombre">Nombre :</label>
+          <input type="text" name="nombre" id="nombre">
+          <input type="submit" value="Enviar">
+   </form>
+</body>
+</html>
