ee32362e1d0876976a1280691db9215da9487a40
Yokabeth Valdes <djb502660@gmail.com>
2025-10-23 21:50:04 -0500

    Trabajo index

---
+++ VALDES JARAMILLO/Practica.php/Index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Inicio</title>
+    <style>
+        h1 {
+            color: blue; 
+        }
+    </style>
+</head>
+<body>
+    <h1>Bienvenido a mi página</h1>
+    <p>Este es un archivo index.php sencillo.</p>
+
+    <?php
+    echo "<p>Hoy es " . date("d/m/Y") . "</p>";
+    ?>
+</body>
+</html>
