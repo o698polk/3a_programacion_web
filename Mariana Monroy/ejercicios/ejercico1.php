2aacae24561751b5b0bc4a7e4436a1563090ad1d
MARIANA ELIZABETH MONROY CAMACHO <marianamonroy1993@gmail.com>
2025-10-24 14:56:26 -0500

    Mi primer ejercicio

---
+++ Mariana Monroy/ejercicios/ejercico1.php
@@ -0,0 +1,24 @@
+<?php
+$nombre="MARIANA MONROY";
+$edad="24";
+$imagen="img_9773MARIANA";
+$cedula="0804388569";
+?>
+
+<html>
+<head>
+    <title>EJERCICIO 1</title>
+    <link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+<center>
+<div class="panel_datos">
+ <h2 >DATOS PERSONALES</h2>
+ <img src="<?php echo $imagen ?>" id="imagen_perfil" >
+<p> <strong> NOMBRE:</strong><?php echo $nombre ?></p><br>
+<p> <strong> EDAD:</strong><?php echo $edad ?></p><br>
+</div>
+</center>
+</body>
+</html>
--- Mariana Monroy/proyectos/index.php
+++ Mariana Monroy/proyectos/index.php
@@ -1,4 +1,8 @@
 <?php
+<<<<<<< Updated upstream
 // Ejemplo simple en PHP
+=======
+// i
+>>>>>>> Stashed changes
 echo "¡Hola mundo! Este es mi primer código en PHP.";
 ?>
