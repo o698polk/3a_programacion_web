f62f7f17f6dc93d697d475b70141d25ee0ee0a38
YadiiCabeza96 <yadicabeza12.96@gmail.com>
2025-10-30 18:38:11 -0500

    Nuevo ejercicio

--- Yadira Cabeza/Ejercicios/Ejercicio_1/Index.php
+++ Yadira Cabeza/Ejercicios/Ejercicio_1/Index.php
@@ -1,8 +1,9 @@
   <?php
 $nombre= "YADIRA CABEZA";
-$edad= "29";
+$edad= "29 AÑOS";
 $imagen= "Img/IMG_20250805.jpg";
 $cedula= "1050480000";
+$celular= "0988630000";
 $correo= "yadicabeza12.96@gmail.com"
 ?>
 
@@ -17,10 +18,12 @@ $correo= "yadicabeza12.96@gmail.com"
 <div class="panel_datos">
  <h2 >DATOS PERSONALES</h2>
  <img src="<?php echo $imagen ?>" id="imagen_perfil" >
-<p> <strong> NOMBRE:</strong><?php echo $nombre ?></p><br>
-<p> <strong> EDAD:</strong><?php echo $edad ?></p><br>
-<p> <strong> CEDULA:</strong><?php echo $cedula ?></p><br>
-<p> <strong> CORREO:</strong><?php echo $correo ?></p><br>
+<p> <strong> NOMBRE: </strong><?php echo $nombre ?></p><br>
+<p> <strong> EDAD: </strong><?php echo $edad ?></p><br>
+<p> <strong> CEDULA: </strong><?php echo $cedula ?></p><br>
+<p> <strong> CELULAR: </strong><?php echo $celular ?></p><br>
+<p> <strong> CORREO: </strong><?php echo $correo ?></p><br>
+<button id="boton_saludar" onclick="alert('HOLA <?php echo $nombre ?>, BIENVENIDA A LA PROGRAMACIÓN WEB')">SALUDAR</button>
 </div>
 </center>
 </body>
