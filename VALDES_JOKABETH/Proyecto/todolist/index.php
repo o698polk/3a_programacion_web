fc05f924da5b4492dd2529edf59c06f94c80284b
Yokabeth Valdes <djb502660@gmail.com>
2025-11-06 19:48:17 -0500

    Refactor code structure for improved readability and maintainability

--- VALDES_JOKABETH/Proyecto/todolist/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Redirige inmediatamente a la página de inicio principal.
-// Esto es más eficiente que mostrar una página de carga.
-header('Location: frontend/view/home/Index.php');
-exit();
