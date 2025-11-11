d398471e1cc8d2fdfddcc44e50ced11800814117
MARIANA ELIZABETH MONROY CAMACHO <marianamonroy1993@gmail.com>
2025-11-11 15:33:27 -0500

    MODIFICADO

--- Mariana Monroy/ejercicios/ejercicio 1/ejercico1.php
+++ Mariana Monroy/ejercicios/ejercicio 1/ejercico1.php
@@ -9,20 +9,38 @@ $correo="marianamonroy1993@gmail.com";
 <html>
 <head>
     <title>EJERCICIO 1</title>
-    <link href="style.css" rel="stylesheet">
+   
     <style>
-        .panel_datos{
-   background-color: rgb(236, 104, 197);
-   border-radius: 20px;
-   border:solid 3px black;
-   width: 350px; 
+    .panel_datos{
+   background-color: rgba(247, 127, 197, 1);
+   border-radius: 10px;
+   border:solid 3px rgba(1, 19, 26, 1);
+   width: 300px; 
 }
-.imagen_perfil{
+#imagen_perfil{
     width:80px;
     height: 80px;
     border-radius: 100px;
-    border:solid 2px rgb(148, 11, 130)
+    border:solid 2px rgb(240, 202, 202)
 }
+
+    #boton_saludar{
+        background-color: rgb(129, 0, 75);
+        color: rgb(255, 255, 255);
+        border: none;
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        cursor: pointer;
+        border-radius: 12px;
+        animation: alternate blinker 1s linear infinite;
+}
+.Boton_saludar:hover {
+        background-color: rgb(240, 144, 184);
+    }
     </style>
 </head>
 
@@ -35,6 +53,7 @@ $correo="marianamonroy1993@gmail.com";
 <p> <strong> EDAD:</strong><?php echo $edad ?></p><br>
 <p> <strong> CEDULA:</strong><?php echo $cedula ?></p><br>
 <p> <strong> CORREO:</strong><?php echo $correo ?></p><br>
+<button id="boton_saludar" onclick="alert('HOLA, BIENVENIDO A LA PROGRAMACIÓN WEB')">Mi Perfil</button>
 </div>
 </center>
 </body>
